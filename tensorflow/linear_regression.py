fb392aff33ad77be54a25ac438498a68a697ccd8
cmasch <christopher.masch@gmail.com>
2019-03-11 19:01:31 +0100

    update adding custom layer

--- tensorflow/linear_regression.py
+++ tensorflow/linear_regression.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Example for linear regression in TensorFlow 2
+Example for linear regression in TensorFlow 2.x
 
 @author: Christopher Masch
 """
 
-import tensorflow
+import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,12 +15,32 @@ learning_rate = 0.01
 nb_epochs = 10
 
 # Generating linear data
-X_train = np.arange(10).reshape((10,1))
-y_train = np.array([i for i in[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]])
+X_train = np.arange(10).reshape((10, 1))
+y_train = np.array([i for i in [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]])
 
-model = keras.Sequential([
-    keras.layers.Dense(10, activation='relu', input_shape=[1]),
-    keras.layers.Dense(1)
+
+# Building linear layer
+class LinearLayer(tf.keras.layers.Layer):
+
+    def __init__(self, *args, **kwargs):
+        super(LinearLayer, self).__init__(*args, **kwargs)
+
+    def build(self, input_shape):
+        self.w = self.add_weight(
+            initializer=tf.keras.initializers.zeros(),  # Dont use ones!
+            trainable=True,
+            name='weight'
+        )
+
+    @tf.function
+    def call(self, inputs):
+        return inputs * self.w
+
+
+model = tf.keras.Sequential([
+    LinearLayer(input_shape=(1,)),
+    # keras.layers.Dense(10), # you can also add Dense layer
+    # keras.layers.Dense(1),
 ])
 
 optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
@@ -43,8 +63,9 @@ plt.plot(X_train, model.predict(X_train), 'r', label='Regression model')
 plt.legend()
 plt.show()
 
-plt.plot(range(1, len(history.history['loss'])+1), history.history['loss'])
+plt.plot(range(1, len(history.history['loss']) + 1), history.history['loss'])
 plt.tight_layout()
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.show()
+
